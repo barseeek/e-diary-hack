553d7809e1c2a40e881ecbf9fd839600865705fc
Nikita <barskii.no@outlook.com>
2023-12-11 14:03:04 +0300

    Reduced the number of database queries, add function to count it, add exceptions

--- functions.py
+++ functions.py
@@ -4,82 +4,98 @@ from datacenter.models import Chastisement
 from datacenter.models import Commendation
 from datacenter.models import Lesson
 from datacenter.models import Mark
-from datacenter.models import Subject
+from django.db import connection, reset_queries
+
+
+PHRASES = [
+    "Молодец!",
+    "Отлично!",
+    "Хорошо!",
+    "Гораздо лучше, чем я ожидал!",
+    "Ты меня приятно удивил!",
+    "Великолепно!",
+    "Прекрасно!",
+    "Ты меня очень обрадовал!",
+    "Именно этого я давно ждал от тебя!",
+    "Сказано здорово – просто и ясно!",
+    "Ты, как всегда, точен!",
+    "Очень хороший ответ!",
+    "Талантливо!",
+    "Ты сегодня прыгнул выше головы!",
+    "Я поражен!",
+    "Уже существенно лучше!",
+    "Потрясающе!",
+    "Замечательно!",
+    "Прекрасное начало!",
+    "Так держать!",
+    "Ты на верном пути!",
+    "Здорово!",
+    "Это как раз то, что нужно!",
+    "Я тобой горжусь!",
+    "С каждым разом у тебя получается всё лучше!",
+    "Мы с тобой не зря поработали!",
+    "Я вижу, как ты стараешься!",
+    "Ты растешь над собой!",
+    "Ты многое сделал, я это вижу!",
+    "Теперь у тебя точно все получится!"
+]
+
+
+def num_queries(reset=True):
+    print(len(connection.queries))
+    if reset:
+        reset_queries()
+
+
+def get_schoolkid(fullname: str):
+    try:
+        return Schoolkid.objects.get(full_name__contains=fullname)
+    except Schoolkid.DoesNotExist:
+        raise Exception(
+            "Не нашел ученика, проверьте корректность данных"
+        )
+    except Schoolkid.MultipleObjectsReturned:
+        raise Exception(
+            "Нашел больше одного ученика, проверьте корректность данных"
+        )
 
 
 def fix_marks(schoolkid: Schoolkid):
     bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    if bad_marks.count() == 0:
+    if bad_marks.exists():
+        return bad_marks.update(points=5)
+    else:
         raise Exception(
             "Не нашел плохих оценок для ученика, проверьте корректность данных"
         )
-    for mark in bad_marks:
-        mark.points = 5
-        mark.save()
-    return bad_marks.count()
 
 
 def remove_chastisements(schoolkid: Schoolkid):
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    if chastisements.count() == 0:
+    if chastisements.exists():
+        return chastisements.delete()
+    else:
         raise Exception(
             "Не нашел замечаний для ученика, проверьте корректность данных"
         )
-    for chastisement in chastisements:
-        chastisement.delete()
 
 
 def create_commendation(fullname: str, subject_name: str):
-    phrases = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!",
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Очень хороший ответ!",
-        "Талантливо!",
-        "Ты сегодня прыгнул выше головы!",
-        "Я поражен!",
-        "Уже существенно лучше!",
-        "Потрясающе!",
-        "Замечательно!",
-        "Прекрасное начало!",
-        "Так держать!",
-        "Ты на верном пути!",
-        "Здорово!",
-        "Это как раз то, что нужно!",
-        "Я тобой горжусь!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!",
-        "Ты растешь над собой!",
-        "Ты многое сделал, я это вижу!",
-        "Теперь у тебя точно все получится!"
-    ]
-    try:
-        kid = Schoolkid.objects.get(full_name__contains=fullname)
-        subject = Subject.objects.get(
-            title=subject_name,
-            year_of_study=kid.year_of_study
-        )
-        subject_lessons = Lesson.objects.filter(
-            subject=subject,
-            group_letter=kid.group_letter,
-            year_of_study=kid.year_of_study
-        )
+    kid = get_schoolkid(fullname)
+    random_subject_lesson = Lesson.objects.filter(
+        subject__title=subject_name,
+        group_letter=kid.group_letter,
+        year_of_study=kid.year_of_study
+    ).order_by('?').first()
+    if random_subject_lesson:
         return Commendation.objects.create(
-            text=random.choice(phrases),
-            created=subject_lessons.first().date,
+            text=random.choice(PHRASES),
+            created=random_subject_lesson.date,
             schoolkid=kid,
-            subject=subject, teacher=subject_lessons.first().teacher
+            subject=random_subject_lesson.subject,
+            teacher=random_subject_lesson.teacher
         )
-    except Schoolkid.DoesNotExist:
-        print("Не могу найти ученика, проверьте корректность ФИО")
-    except Subject.DoesNotExist:
-        print("Не могу найти предмет, проверьте корректность ввода")
+    else:
+        raise Exception(
+            "Не могу найти урок по предмету, проверьте корректность ввода"
+        )
